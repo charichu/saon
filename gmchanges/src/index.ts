1b417d41229ef2368e3821c3260c4cae904f908c
charichu <32455463+charichu@users.noreply.github.com>
2020-11-10 23:16:35 +0100

    test specific testing

--- gmchanges/src/index.ts
+++ gmchanges/src/index.ts
@@ -8,7 +8,7 @@ import { ExpirationCompleteListener } from './events/listeners/expirationComplet
 import { PaymentCreatedListener } from './events/listeners/paymentCreatedListener';
 
 const start = async () => {
-  console.log('Starting gmchanges....');
+  console.log('Starting gmchanges...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
