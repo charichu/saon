612945f40b6d53be26c198f4afeb7516e378ed32
charichu <32455463+charichu@users.noreply.github.com>
2020-12-03 04:41:07 +0100

    For the love of god...

--- characters/src/helper/getForExpose/getEquippedArmor.ts
+++ characters/src/helper/getForExpose/getEquippedArmor.ts
@@ -3,13 +3,16 @@ import { getInventory } from './getInventory';
 export async function getEquippedArmor(character: any) {
 
     const items = await getInventory(character);
-    let equippedArmor = new Array();    
+    let equippedArmor = new Array();
+    
+    try{
 
     for (var key of Object.keys(items)) {
         if(items[key].pro){
             equippedArmor.push(items[key]);
         }
     }
+    }catch(e){console.log(e)};
 
     let armor = new Array();
 
